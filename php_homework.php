89083e222929ceb07fb3539203cf32cb7b6205be
Martalepin <marta.l.pineda@pace.edu>
2019-05-31 20:19:13 -0400

    Update php_homework.php

--- php_homework.php
+++ php_homework.php
@@ -4,20 +4,20 @@
  </head>
  <body>
  <?php 
-		$x = 1.50;
+		$x = 1.50; //variable x to store a value
 		$y = 1.50;
-		$total = $x + $y;
+		$total = $x + $y; //variable total to store the addition of the values on variables x and y
 		
 		
-		$brk = "empanadas";
+		$brk = "empanadas"; 
 		$brkd = "coffee";
 		
 		echo "I love " . $brk . " and " . $brkd ." for my breakfast everyday! ";
-		echo  "so, I spend " . $total ." dollars every morning!";
+		echo  "so, I spend " . $total ." dollars every morning!"; 
 		echo "<br>";
 		echo "<br>";
 		
-		$t = date("H");
+		$t = date("H"); //Creating a variable t that stores the value of time, pulled from the server. date is a method buitl in PHP to retrieve the date from the server
 		if($t % 2 ){ 
 				echo "Check your time, this is an even hour!";  
 			} else { 
